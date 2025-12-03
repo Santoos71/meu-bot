7f272009a35e0e0e41e67226e587f26854e3920c
Filipe Santos <empreendedordigital.lipe71@gmail.com>
2025-12-03 15:20:24 -0300

    configurar o Gunicorn

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot está rodando!"
+    return "Bot está rodando com Gunicorn!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
